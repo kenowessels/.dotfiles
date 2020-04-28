f6bed2de19ad28b7f4dc8458044d986a56a6eaae
Keno Wessels <k.wessels@demv.de>
2020-04-28 17:12:53 +0200

    Added more aliases to config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,11 +8,22 @@ alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 ### Aliases
 
+## Git
+
+# Remove old local-branches
+alias gbpurge='git branch --merged | grep -Ev "(\*|master|develop)" | xargs -n 1 git branch -d'
+
 ## Docker 
 
-# Start & Stop
+# Docker Compose Start & Stop
 alias dup='docker-compose up -d'
 alias dstop='docker-compose stop'
 
-# Execute
+# Docker Compose Execute
 alias dex='docker-compose exec'
+
+# Docker Compose execute composer inside php-fpm 
+alias dexcomposer='docker-compose exec php-fpm composer'
+
+# Docker Compose execute artisan inside php-fpm
+alias dexartisan='docker-compose exec php-fpm php artisan'
